7623e6eaa56915702824793e58691017d4a4ef28
PaleBlueDotTech <2519893152@qq.com>
2022-05-05 15:24:16 +0800

    feat: loop

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,35 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const confreenceTickets = 50 //不变的变量
 	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking applicatio.\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are availible\n", confreenceTickets, remainingTickets)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// 输入用户名
-	fmt.Println(("Enter your fist name: "))
-	fmt.Scan(&firstName)
+	for {
 
-	fmt.Println(("Enter your last name: "))
-	fmt.Scan(&lastName)
+		// var bookings []string         //array
 
-	fmt.Println(("Enter number of tickets: "))
-	fmt.Scan(&userTickets)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println(("Enter your email: "))
-	fmt.Scan(&email)
+		// 输入用户名
+		fmt.Println(("Enter your fist name: "))
+		fmt.Scan(&firstName)
 
-	remainingTickets = remainingTickets - userTickets
+		fmt.Println(("Enter your last name: "))
+		fmt.Scan(&lastName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v \n",remainingTickets, conferenceName)
+		fmt.Println(("Enter number of tickets: "))
+		fmt.Scan(&userTickets)
+
+		if userTickets <= remainingTickets {
+			fmt.Println(("Enter your email: "))
+			fmt.Scan(&email)
+
+			// fmt.Printf("The whole slice: %v\n", bookings)
+			// fmt.Printf("The first value: %v\n", bookings[0])
+			// fmt.Printf("The type of slice %T\n", bookings)
+			// fmt.Printf("The lenth of slice %v\n", len(bookings))
+
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				names := strings.Fields(booking) //strings.Field() 可以根据空格进行分片
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first name of bookings are: %v\n", firstNames)
+
+			var noTickets bool = remainingTickets == 0
+			if noTickets {
+				fmt.Println("Booked out. Come back next year.")
+				break
+			}
+		} else {
+			fmt.Printf("Sorry,Remaining Tickets are only %v, so you can't book %v tickets\n", remainingTickets, userTickets)
+			continue
+
+		}
+	}
 
 }
