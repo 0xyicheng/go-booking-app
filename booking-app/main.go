e9ae2202fb42bd18d956405ab5f9ee7ca078236c
PaleBlueDotTech <2519893152@qq.com>
2022-05-07 13:00:28 +0800

    feat：split multiple file

--- booking-app/main.go
+++ booking-app/main.go
@@ -57,12 +57,7 @@ func getFirstNames() []string {
 	}
 	return firstNames
 }
-func ValidateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
-	var isValidEmail bool = strings.Contains(email, "@")
-	var isValidTicket bool = userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicket
-}
+
 func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
